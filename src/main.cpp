17673f2487bacdcd54af510aea658957533bfb07
Nils Niemann <nils.niemann@dfki.de>
2020-05-16 16:42:38 +0200

    setting fonts

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,32 @@
 #include <QtCore>
 #include <QApplication>
 
+#include <QFont>
+#include <QFontDatabase>
+
+#include <QDebug>
+
+#include <iostream>
+
 int main(int argc, char** args)
 {
     QApplication app(argc, args);
+
+    QFontDatabase fontdb;
+
+#ifdef _WIN32
+    // select the system-default monospace font
+    QFont font = fontdb.systemFont(QFontDatabase::FixedFont);
+    font.setPointSize(12);
+
+    std::cout << font.family().toStdString() << ", " << font.pointSize() << std::endl;
+
+    // set it as default for the application
+    app.setFont(font);
+#else
+    QFont font = fontdb.font("Ubuntu Mono", "Normal", 12);
+    app.setFont(font);
+#endif
 
     CounterWidget widget;
     widget.show();
